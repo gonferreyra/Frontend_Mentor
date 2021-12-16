d1acef2fee331444050dfcb448e75b929a971902
Gonzalo Ferreyra <gonferreyra@gmail.com>
2021-12-16 19:24:23 -0300

    faq-acordion starting desktop

--- faq-accordion-card-main/js/app.js
+++ faq-accordion-card-main/js/app.js
@@ -1,8 +1,18 @@
-function myFunction() {
+function myFunctionDown() {
     var x = document.getElementById("hidden");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+    x.classList.toggle("text-hidden");
+    // if (x.style.display === "none") {
+    //   x.style.display = "block";
+    // } else {
+    //   x.style.display = "none";
+    // } 
+    // e.classList.toggle("open");
+    // console.log(e);
+    var y = document.getElementById("btn-1");
+    y.classList.toggle("open");
   }
+
+function rotate() {
+  var y = document.getElementById("btn-1");
+  y.classList.toggle("open");
+}
