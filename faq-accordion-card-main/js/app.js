5d742b9bf03ae060a275de5591c9fd6a93f7dd76
Gonzalo Ferreyra <gonferreyra@gmail.com>
2021-12-21 20:37:32 -0300

    faq card

--- faq-accordion-card-main/js/app.js
+++ faq-accordion-card-main/js/app.js
@@ -1,18 +1,43 @@
 function myFunctionDown() {
     var x = document.getElementById("hidden");
-    x.classList.toggle("text-hidden");
-    // if (x.style.display === "none") {
-    //   x.style.display = "block";
-    // } else {
-    //   x.style.display = "none";
-    // } 
-    // e.classList.toggle("open");
-    // console.log(e);
+    x.classList.toggle("text-hidden")
     var y = document.getElementById("btn-1");
     y.classList.toggle("open");
+    var z = document.getElementById("btn-1");
+    z.classList.toggle("btn-bold");
   }
 
-function rotate() {
-  var y = document.getElementById("btn-1");
-  y.classList.toggle("open");
-}
+  // La primera variable busca en el documento (html) el ID "hidden", y con el classlist.toggle agrega o quita la CLASS "text-hidden";
+  // La segunda variable busca en el documento (html) la ID "btn-1", y agrega o quita la CLASS "open".-
+  function myFunctionDown2() {
+    var x = document.getElementById("hidden-2");
+    x.classList.toggle("text-hidden")
+    var y = document.getElementById("btn-2");
+    y.classList.toggle("open");
+    var z = document.getElementById("btn-2");
+    z.classList.toggle("btn-bold");
+  }
+  function myFunctionDown3() {
+    var x = document.getElementById("hidden-3");
+    x.classList.toggle("text-hidden")
+    var y = document.getElementById("btn-3");
+    y.classList.toggle("open");
+    var z = document.getElementById("btn-3");
+    z.classList.toggle("btn-bold");
+  }
+  function myFunctionDown4() {
+    var x = document.getElementById("hidden-4");
+    x.classList.toggle("text-hidden")
+    var y = document.getElementById("btn-4");
+    y.classList.toggle("open");
+    var z = document.getElementById("btn-4");
+    z.classList.toggle("btn-bold");
+  }
+  function myFunctionDown5() {
+    var x = document.getElementById("hidden-5");
+    x.classList.toggle("text-hidden")
+    var y = document.getElementById("btn-5");
+    y.classList.toggle("open");
+    var z = document.getElementById("btn-5");
+    z.classList.toggle("btn-bold");
+  }
